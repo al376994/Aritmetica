0db2b81737d4d43de5f98a6ae3b0940cb17d31aa
Daniel Palau Ruzafa <al376994@alib0035act.sg.uji.es>
2019-02-05 15:34:10 +0100

    Como controlo las excepciones?

--- src/main/java/ecuacion/EcuacionCuadratica.java
+++ src/main/java/ecuacion/EcuacionCuadratica.java
@@ -1,4 +1,34 @@
 package ecuacion;
 
+import static java.lang.Math.sqrt;
+
 public class EcuacionCuadratica {
+
+    double a;
+    double b;
+    double c;
+
+    public EcuacionCuadratica(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double resolver() {
+        if (c == 0) {
+            return 0;
+        }
+        if (a == 0) {
+            EcuacionLineal ecuacionLineal = new EcuacionLineal(b, c);
+            return ecuacionLineal.resolver();
+        }
+        if (b == 0) {
+            EcuacionLineal ecuacionLineal = new EcuacionLineal(a, c);
+            double x = ecuacionLineal.resolver();
+            try {
+                return sqrt(x);
+            } catch (UnsupportedOperationException)
+
+        }
+    }
 }
--- src/test/java/ecuaciones/EcuacionCuadraticaTest.java
+++ src/test/java/ecuaciones/EcuacionCuadraticaTest.java
@@ -1,4 +1,17 @@
 package ecuaciones;
 
+import ecuacion.EcuacionCuadratica;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class EcuacionCuadraticaTest {
+    EcuacionCuadratica ecuacionCuadratica;
+
+    @Test
+    public void terminoIndependienteCeroTest() {
+        ecuacionCuadratica = new EcuacionCuadratica(1.0, 0.0);
+        assertThat(ecuacionCuadratica.resolver(), is(0.0));
+    }
 }
